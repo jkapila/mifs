a4116ed1b7406478d7b89b20fb2a088ac87e67c7
Jitin Kapila <jitin.kapila@gmail.com>
2016-09-04 21:14:21 +0530

    importing error resolved

---
+++ __init__.py
@@ -0,0 +1,7 @@
+_name__ = "mifs"
+__doc__ = "Parallalized MRMR in python"
+__author__ = 'Daniel Homola'
+__version__ = "0.0.1"
+__maintainer__ = "Jitin Kapila"
+__status__ = "Alpha"
+__all__ = ['mifs']
